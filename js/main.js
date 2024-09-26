07b5bd41ca94c705c40eaa605ab3bfd9571bf7a4
VINITPANDEY09 <vinitcodez@gmail.com>
2024-09-26 11:07:02 +0530

    new file:   .vscode/settings.json 	new file:   css/slider.css 	new file:   css/style.css 	new file:   css/swiper-buddle.min.css 	new file:   images/g1.webp 	new file:   images/g2.webp 	new file:   images/g3.webp 	new file:   images/g4.webp 	new file:   images/g5.webp 	new file:   images/g6.webp 	new file:   images/image-1.jpg 	new file:   images/image-2.jpg 	new file:   images/image-4.webp 	new file:   images/image-5.webp 	new file:   inderx.html 	new file:   js/main.js 	new file:   js/swiper-buddle.min.js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const swiperEl = document.querySelector('swiper-container')
+Object.assign(swiperEl, {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    clickable: true,
+  },
+  freeMode: true,
+  loop: true,
+  allowTouchMove: false,
+  autoplay: {
+  delay: 0,
+  disableOnInteraction: false,
+  },
+  slidesPerView: 5.5,
+  spaceBetween: 32,
+  speed: 10000,
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
+
+swiperEl.initialize();
